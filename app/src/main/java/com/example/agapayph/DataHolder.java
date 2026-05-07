74574b7cc9d2931a314a168da9a408f3bc8d1787
ProfSavage <dejesusmykelouies@gmail.com>
2026-05-07 23:55:48 +0800

    Myke - Simula ng Log In at Sign Up Tulog muna me

---
+++ app/src/main/java/com/example/agapayph/DataHolder.java
@@ -0,0 +1,9 @@
+package com.example.agapayph;
+
+public class DataHolder {
+
+    public static String username;
+    public static String role;
+
+
+}
--- app/src/main/java/com/example/agapayph/MainActivity.java
+++ app/src/main/java/com/example/agapayph/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.agapayph;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences sp;
+    SharedPreferences.Editor editor;
+    Boolean status;
+    String inputUsername, inputPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,52 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+
+        sp = getSharedPreferences("MyPref", MODE_PRIVATE);
+
+        DataHolder.username = sp.getString("username", "None");
+        DataHolder.role = sp.getString("role", "None");
+        status = sp.getBoolean("isLoggedIn", false);
+
+    }
+
+    public void logInClicked(View view) {
+
+        EditText etUsername = findViewById(R.id.editTextText2);
+        EditText etPassword = findViewById(R.id.editTextTextPassword);
+
+        inputUsername = etUsername.getText().toString();
+        inputPassword = etPassword.getText().toString();
+
+        // LOGIC FOR CHECKING USERNAME AND PASSWORD
+
+        Boolean result = false;
+
+        // LOGIC FOR CHECKING USERNAME AND PASSWORD
+        // Pag nakapaglogin si user, ilagay sa DataHolder.role yung role nya, diko alam kung pano HAHAHAAHAHHAHA
+
+        if (result) {
+
+
+            //Kapag Successful log in
+            editor = sp.edit();
+            editor.putString("username", inputUsername);
+            editor.putString("role", DataHolder.role);
+            editor.putBoolean("isLoggedIn", true);
+
+
+        } else {
+
+            Toast.makeText(this, "Incorrect email or password", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+
+    public void registerClicked(View view) {
+        Intent i = new Intent(this, SignUp.class);
+        startActivity(i);
     }
 }
